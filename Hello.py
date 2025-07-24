7b7647b25e221aa929a270a0701eeb0002c3fcdf
ksg-dev <ksg.dev.data@gmail.com>
2025-07-24 09:54:23 -0500

    better layout on hello page

--- Hello.py
+++ Hello.py
@@ -17,33 +17,43 @@ st.markdown(
     
     ### Choose Your Buddy
     Different buddies for different needs.
+    """)
 
+st.markdown(
+    """
     #### 📍 Geo Buddy: Address Geocoder
     *Uses geopy Library*
 
     *Only works with Raleigh, NC addresses currently*
 
     ##### :material/checklist: Use Case
-    You have: `csv` with `address` column.
-    and you need: `csv` with cooresponding `latitude` and `longitude` columns
+    :green-badge[:material/task_alt: You have] `csv` with `address` column.\n
+    :orange-badge[:material/pending: You need] `csv` with cooresponding `latitude` and `longitude` columns
 
+    ##### :material/info: About
     Give it a `csv` file with an `address` column and use geopy to add `latitude` and `longitude`.
 
     Then export the results as a `csv` that can be added as a layer in an ArcGIS map.
+"""
+)
 
-    ---
+st.divider()
 
+    
+st.markdown(
+    """
     #### 📌 Overpass Buddy: Place Finder
     *Uses OpenStreetMap Overpass API*
 
     ##### :material/checklist: Use Case
-    :material/task_alt: You have: `city` and what you want to `filter` by (Restaurants, Libraries, etc.)\n
-    :material/pending: and you need: `csv` with geocoded locations matching your criteria, 
+    :green-badge[:material/task_alt: You have] `city` and what you want to `filter` by (`Restaurants`, `Libraries`, etc.)\n
+    :orange-badge[:material/pending: You need] `csv` with geocoded locations matching your criteria, 
     
+    ##### :material/info: About
     Enter a `city` name, and choose from defined `filters` like `Libraries`, `Restaurants`, or `Hotels`,
     or browse OpenStreetMap map features for `custom filtering` options.
 
-    Then export the results as a `csv` that can be added as a layer in an ArcGIS map.
-
-    """
+    Then export the results as a `csv` that can be added as a layer in an ArcGIS map."""
 )
+
+    
